bdac52db516d3cd5361864bc96e01eb81ad95740
HY Yin <haoyun.tw@gmail.com>
2020-07-04 19:18:01 +0800

    Add files via upload

--- Python/264.nthUglyNumber.py
+++ Python/264.nthUglyNumber.py
@@ -1,38 +1,17 @@
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
-        l2 = [2]
-        l3 = [3]
-        l5 = [5]
         ugly_nums = [1]
-        count = 1
-        while count != n:
-            v2 = l2[0]
-            v3 = l3[0]
-            v5 = l5[0]
-            count += 1
-            if v2 < v3:
-                if v2 < v5:
-                    del l2[0]
-                    next_ugly = v2
-                else:
-                    del l5[0]
-                    next_ugly = v5
-            else:
-                if v3 < v5:
-                    del l3[0]
-                    next_ugly = v3
-                else:
-                    del l5[0]
-                    next_ugly = v5
-
-            if next_ugly * 2 not in l3 and next_ugly * 2 not in l5:
-                l2.append(next_ugly * 2)
-            if next_ugly * 3 not in l2 and next_ugly * 3 not in l5:
-                l3.append(next_ugly * 3)
-            if next_ugly * 5 not in l2 and next_ugly * 5 not in l3:
-                l5.append(next_ugly * 5)
+        i2 = i3 = i5 = 0  # index
+        while len(ugly_nums) != n:
+            next_ugly = min(ugly_nums[i2] * 2, ugly_nums[i3] * 3, ugly_nums[i5] * 5)
+            if next_ugly == ugly_nums[i2] * 2:
+                i2 += 1
+            if next_ugly == ugly_nums[i3] * 3:
+                i3 += 1
+            if next_ugly == ugly_nums[i5] * 5:
+                i5 += 1
             ugly_nums.append(next_ugly)
-            print(ugly_nums, l2, l3, l5)
+            print(ugly_nums, i2, i3, i5)
         return ugly_nums[n-1]
 
 s = Solution()
