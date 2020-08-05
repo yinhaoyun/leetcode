bf1e433062fd515a7b5cc48ddec77f39e432afb7
haoyun <haoyun.tw@gmail.com>
2020-08-05 22:52:07 +0800

    Improve

--- Python/211.add-and-search-word-data-structure-design.py
+++ Python/211.add-and-search-word-data-structure-design.py
@@ -1,33 +1,32 @@
-import collections
-
-
 class WordDictionary:
     def __init__(self):
-        self.children = collections.defaultdict(WordDictionary)
+        self.children = dict()
+        self.is_end = False
 
     def addWord(self, word: str) -> None:
-        if len(word) == 0:
-            self.children[""] = WordDictionary()
-            return
-        self.children[word[0]].addWord(word[1:])
+        cur = self
+        for c in word:
+            cur = cur.children.setdefault(c, WordDictionary())
+        cur.is_end = True
 
     def search(self, word: str) -> bool:
+        # print("search: ", word)
         if len(word) == 0:
-            return self.children.get("") is not None
+            return self.is_end
         if word[0] == ".":
-            for key, trie in self.children.items():
+            for trie in self.children.values():
                 if trie.search(word[1:]):
                     return True
-        elif not self.children.get(word[0]):
-            return False
-        return self.children[word[0]].search(word[1:])
+        elif word[0] in self.children:
+            return self.children[word[0]].search(word[1:])
+        return False
 
 
 w = WordDictionary()
 w.addWord("bad")
 w.addWord("dad")
 w.addWord("mad")
-print(w.search("pad"))
-print(w.search("bad"))
-print(w.search(".ad"))
-print(w.search("b.."))
+print(w.search("pad"))  # False
+print(w.search("bad"))  # True
+print(w.search(".ad"))  # True
+print(w.search("b.."))  # True
