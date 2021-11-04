5b56dd32506d499829c5e1fb7764bcff20d79111
Hao-Yun Yin <haoyun.tw@gmail.com>
2021-11-04 20:31:58 +0800

    35. Search Insert Position

---
+++ Python/35.SearchInsertPosition.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if target < nums[mid]:
+                right = mid - 1
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                return mid
+        return left
